eb3576de0d5af1c4977860c48a11be5ff45e2249
Marko <marko@pepega.club>
2022-10-19 10:31:49 +0200

    Update the example config

--- config.example.mjs
+++ config.example.mjs
@@ -1,5 +1,5 @@
 export default {
-	repo: "https://github.com/33kk/supibot-aliases",
-	github: "Token with gists scope https://github.com/settings/tokens",
-	supibot: "userId:token from https://supinic.com/user/auth-key"
+	repo: "https://github.com/33kk/supibot-aliases", // The repo which will be linked in gist descriptions
+	github: "ghp_ADAPOgeoiIOSDHUIEakdNSDFoihSDFIUadfD", // Get the token from https://github.com/settings/tokens with "gists" scope selected
+	supibot: "userId:token" // Get the token and userId from https://supinic.com/user/auth-key
 }
